bd3b8bacd4920cd16c0b8a29468d13293dc70aef
이정우 <ljw3771@kakao.com>
2022-11-22 11:53:20 +0900

    initial setting

---
+++ leeJungWoo/app.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+import mysql from 'mysql2';
+
+dotenv.config();
+
+const myDatasource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+});
+myDatasource
+  .initialize() //
+  .then(() => {
+    console.log('connected!');
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
+
+const PORT = process.env.PORT;
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`server started at ${PORT}`);
+    });
+  } catch {
+    console.error(err);
+  }
+};
+
+start();
---
+++ leeJungWoo/db/migrations/20221122023405_create_posts_table.sql
@@ -0,0 +1,14 @@
+-- migrate:up
+CREATE TABLE `posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `content` varchar(3000) DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
+-- migrate:down
+
