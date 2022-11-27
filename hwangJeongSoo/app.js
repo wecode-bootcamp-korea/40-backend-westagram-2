c75ea3aaa62f2c77a4acc4cda1b4d1c6f97430d9
JeongSooHHH <space2577@gmail.com>
2022-11-27 16:52:41 +0900

    [40기 - 황정수] Assignment 3: Express API -CRUD

--- hwangJeongSoo/app.js
+++ hwangJeongSoo/app.js
@@ -18,7 +18,7 @@ const appDataSource = new DataSource({
 appDataSource.initialize() 
     .then(()=>{
         console.log("Data Source has been initialized")
-    })
+})
 
 const app = express();
 
@@ -28,12 +28,11 @@ app.use(morgan('dev'))
 
 app.get("/ping", (req, res) =>{
 	res.status(201).json({ message : "pong"});
-    });
+});
 
-
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓2.회원가입하기 : 완료
 app.post("/signup", async(req, res, next)=>{
     const { name, email, profile_image, password } = req.body
-
 
 await appDataSource.query(
     `INSERT INTO users(
@@ -46,7 +45,118 @@ await appDataSource.query(
     [ name, email, profile_image, password ]
 ); 
      res.status(201).json({ message : "successfully created"});
+})
+
+app.post("/posts", async(req, res, next)=>{
+    const { title, content, user_id } = req.body
+        await appDataSource.query(
+    `INSERT INTO posts(
+        title,
+        content,
+        user_id
+    ) VALUES (?, ?, ?);
+    `,
+    [ title, content, user_id]
+); 
+     res.status(201).json({ message : "postCreated"});
+})
+
+
+app.get('/posts', async(req, res, next) => {
+    await appDataSource.query(
+        `
+        SELECT 
+        u.id as userId, 
+        u.profile_Image as userProfileImage, 
+        p.id as postingId,
+        p.image_Url as postingImageUrl,
+        p.content as postingContent
+        FROM users as u 
+        RIGHT JOIN posts as p
+        ON p.id = u.id
+                
+    ;`,
+
+        (err ,rows) => {
+        res.status(201).json({data: rows});
+        }
+    )
+});  
+
+
+app.get('/user_posts', async(req, res, next) => {
+    const { id } = req.body;
+    await appDataSource.query(
+    `SELECT users.id as userID, users.profile_image as userProfileImage,  
+    
+    JSON_OBJECTAGG(
+    'PostingID', posts.id, 
+    'postingImageUrl', posts.image_url, 
+    'PostingContent',posts.content
+    ) as postings   
+    FROM posts
+    INNER JOIN users
+    ON users.id = ${id}
+    GROUP BY users.id`,
+
+     function(err ,rows) {
+        res.status(200).json({data: rows});
+    }
+    )
+});
+
+app.patch('/put', async(req, res) => {
+    const { content, id } = req.body;
+    
+    await appDataSource.query(
+
+        `UPDATE posts SET content = ? 
+        WHERE id = ${id}
+        ;`,
+      
+      [content]);
+        
+     await appDataSource.query(
+        ` SELECT 
+          u.id as userId, 
+          u.profile_Image as userProfileImage, 
+          p.id as postingId,
+          p.image_Url as postingImageUrl,
+          p.content as postingContent
+          FROM users as u
+          LEFT JOIN posts as p
+          ON p.id = u.id 
+          WHERE p.id = ${id} 
+          ;`,  
+     (err ,rows) => {
+        res.status(201).json({data: rows});      
     })
+})
+
+app.delete('/delete', async(req, res) => {
+    const { id } = req.body
+    
+    await appDataSource.query(
+      `DELETE FROM posts WHERE id = ${id} `
+      );
+  
+    res.status(200).json({ message : "postingDeleted"});
+})
+
+app.post("/likes", async(req, res, next)=>{
+    const { id, user_id, post_id } = req.body
+
+    await appDataSource.query(
+        `INSERT INTO likes( 
+        id,
+        user_id,
+        post_id
+        ) VALUES (${id}, ${user_id},${post_id});
+        `,
+        
+        ); 
+        res.status(201).json({ message : "likeCreated"});
+})
 
 const server = http.createServer(app)
 const PORT = process.env.PORT;
@@ -56,7 +166,6 @@ const start = async () => {
         server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
 }   catch (err) {
     console.error(err);
-}
-};
+}};
 
 start()
