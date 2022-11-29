44973742a7fd3c702bc76142cdeedde58f653a33
minsik <minsik@minsiks-MacBook-Pro.local>
2022-11-29 21:55:37 +0900

    [40기 - 전민식] Assignment 2: Express API - login endpoint

---
+++ hwangJeongSoo/db/migrations/20221124072120_create_likes_table.sql
@@ -0,0 +1,14 @@
+-- migrate:up
+CREATE TABLE
+    likes (
+        id INT NOT NULL,
+        user_id INT NOT NULL,
+        post_id INT NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (user_id) REFERENCES users (id),
+        FOREIGN KEY (post_id) REFERENCES posts (id),
+        CONSTRAINT likes_user_id_ukey UNIQUE (user_id)
+    );
+
+-- migrate:down
+DROP TABLE likes;
---
+++ leeJungWoo/db/migrations/20221122023417_create_likes_table.sql
@@ -0,0 +1,15 @@
+-- migrate:up
+
+CREATE TABLE `likes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `post_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+);
+
+-- migrate:down
+
