455ae5d9c565b600dfe6c840b906e6e45dba0284
이정우 <ljw3771@kakao.com>
2022-11-30 12:22:47 +0900

    [40기 - 이정우] Assignment 3~8: Express -crud validation  미들웨어 수정

--- leeJungWoo/middleware/validation.js
+++ leeJungWoo/middleware/validation.js
@@ -5,7 +5,7 @@ const { getPostByPostId } = require('../models/postDao.js');
 dotenv.config();
 
 const validation = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization.split(' ')[1];
   if (!token) {
     return res.status(403).json({ message: 'Invalid Access Token' });
   }
