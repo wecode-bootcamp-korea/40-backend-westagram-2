657a8af8bb773f4f206f5b0b85ef431603743de7
YAE00 <yeallap00@gmai.com>
2022-12-06 21:57:49 +0900

    변수명, 모듈 순서 변경 완료

--- YoonEunAe/app.js
+++ YoonEunAe/app.js
@@ -1,25 +1,24 @@
 const http = require ("http");
+
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require ("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
 const { DataSource } = require('typeorm');
 
-const myDataSource = new DataSource({
-          type: process.env.TYPEORM_CONNECTION,
-          host: process.env.TYPEORM_HOST,
-          port: process.env.TYPEORM_PORT,
-          username: process.env.TYPEORM_USERNAME,
-          password: process.env.TYPEORM_PASSWORD,
-          database: process.env.TYPEORM_DATABASE
+const mysqlDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE
 }); 
 
-myDataSource.initialize()
-      .then(() => { 
-        console.log("Data Source has been initialized!")
+mysqlDataSource.initialize()
+  .then(() => { 
+    console.log("Data Source has been initialized!")
 });
 
 const app = express()
@@ -32,7 +31,7 @@ const server = http.createServer(app)
 const PORT = process.env.PORT;
 
 const start = async () => {
-          server.listen(PORT, () => console.log(`server is listening on ${PORT}`)
+  server.listen(PORT, () => console.log(`server is listening on ${PORT}`)
 )}
 
 start()
